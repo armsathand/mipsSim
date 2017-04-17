33bb7694f67aa29047bcf6eeaf4cf223dd7449c7
armsathand <arm2mill@gmail.com>
2017-04-17 01:07:24 -0500

    Continued worked progressing a bit, this still needs to be cleaned and worked on. Added the test cases to the git as well

--- src/mipsSim.java
+++ src/mipsSim.java
@@ -12,8 +12,8 @@ public class mipsSim {
             System.out.println("NO ARGUMENTS PASSED \n TERMINATING");
             return;
         }
-        int[] mem = new int[1048576];
-        int[] genPop = new int[32];
+        int[] MEM = new int[1048576];
+        int[] R = new int[32];
         int PC;
         int nPC;
         int LO;
@@ -22,11 +22,46 @@ public class mipsSim {
         String mode = args[1];
 
         try {
+
             FileReader fr = new FileReader(args[0]);
             BufferedReader fin = new BufferedReader(fr);
 
             String curline = null;
 
+            while ((curline = fin.readLine()) != null){
+
+                if (curline.startsWith("[")) {
+
+                    curline.trim();
+
+                    String[] parts = curline.split("\\s+");
+
+                    for(int i = 0; i < parts.length; i++){
+
+                        if(parts[i].equals("[PC]")) {
+                            int placeholder = 0;
+                            PC = placeholder;
+                            nPC = placeholder +4;
+                        }
+
+                        if(parts[i].startsWith("[R")){
+                            int placeholder =0;
+                            int register = Integer.parseInt(parts[i].substring(2,parts[i].length()-2));
+                            R[register] = placeholder;
+                        }
+
+                        if(parts[i].startsWith("[0x") ){
+                            String sub = parts[i].substring(3, parts[i].length()-1);
+                            System.out.println(sub);
+                            long a = Long.parseLong(sub, 16);
+                            System.out.println(a);
+                            int placeholder = 0;
+                            //NOT SAFE!
+                            MEM[(int) a] = placeholder;
+                        }
+                    }
+                }
+            }
 
         } catch (IOException e){
                 System.out.println("FILE NOT FOUND \n TERMINATING");
