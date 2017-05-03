db7b01b269aa95e7ab3233ca3ad44d301ce02fbe
armsathand <arm2mill@gmail.com>
2017-05-03 12:21:27 -0500

    wip3bby

--- src/mipsInstruct.java
+++ src/mipsInstruct.java
@@ -1,5 +1,5 @@
 /**
- * Created by armsathand on 4/17/2017.
+ * Created by armsathand(Aaron Miller) on 4/17/2017.
  */
 public class mipsInstruct {
     int inst;
@@ -23,6 +23,7 @@ public class mipsInstruct {
             i = false;
             j = false;
             a = (inst << 5) >> 16 ;
+            b = (inst << 10) >> 11;
             funct = inst << 24;
             immed = 0;
         } else if(opc <= 3){
@@ -31,11 +32,15 @@ public class mipsInstruct {
             j = true;
             funct = 0;
             immed = 0;
+            a = (inst << 5) >> 16 ;
+            b = (inst << 10) >> 11;
             destination = inst << 5;
         } else {
             r = false;
             i = true;
             j = false;
+            a = (inst << 5) >> 16 ;
+            b = (inst << 10) >> 11;
             immed = inst << 14;
             funct = 0;
         }
@@ -145,61 +150,98 @@ public class mipsInstruct {
         system.advance(system.PC & 0xf0000000);
     }
     public void JAL(){
-
+        system.R[31] = system.nPC;
+        system.advance( system.PC & 0xf0000000);
     }
     public void JR(){
-
+        system.advance(system.R[a]);
     }
     public void LB(){
 
     }
     public void LUI(){
-
+        system.R[b] = immed << 16;
+        system.advance();
     }
     public void LW(){
 
     }
     public void MFHI(){
-
+        system.R[destination] = system.HI;
+        system.advance();
     }
     public void MFLO(){
-
+        system.R[destination] = system.LO;
+        system.advance();
     }
     public void MULT(){
-
+        system.LO = system.R[a] * system.R[b];
+        system.advance();
+    }
+    public void NOOP(){
+        system.advance();
     }
     public void OR(){
-
+        system.R[destination] = system.R[a] | system.R[b];
+        system.advance();
     }
     public void ORI(){
-
+        system.R[b] = system.R[a] | system.R[immed];
+        system.advance();
     }
     public void SLL(){
-
+        system.R[destination] = system.R[b] << system.R[5];
+        system.advance();
     }
     public void SLLV(){
-
+        system.R[destination] = system.R[b] << system.R[a];
+        system.advance();
     }
     public void SLT(){
-
+        if(system.R[a] < system.R[b]){
+            system.R[destination] = 1;
+        } else {
+            system.R[destination] = 0;
+        }
+        system.advance();
     }
     public void SLTI(){
-
+        if(system.R[a] < system.R[immed]){
+            system.R[b] = 1;
+        } else {
+            system.R[b] = 0;
+        }
+        system.advance();
     }
     public void SLTIU(){
-
+        if(system.R[a] < system.R[immed]){
+            system.R[b] = 1;
+        } else {
+            system.R[b] = 0;
+        }
+        system.advance();
     }
     public void SLTU(){
-
+        if(system.R[b] < system.R[a]){
+            system.R[destination] = 1;
+        } else {
+            system.R[destination] = 0;
+        }
+        system.advance();
     }
     public void SRA(){
-
+        ///NEEDS OTHER WORK TO BE DONE
+        system.R[destination] = system.R[b] >> system.R[a];
+        system.advance();
     }
     public void SRL(){
-
+        ///NEEDS OTHER WORK TO BE DONE
+        system.R[destination] = system.R[b] >> system.R[a];
+        system.advance();
     }
     public void SRLV(){
-
+        system.R[destination] = system.R[b] >> system.R[a];
+        system.advance();
     }
     public void sub(){
         system.R[destination] = system.R[a] - system.R[b];
@@ -219,11 +261,14 @@ public class mipsInstruct {
     }
     public void SYSCALL(){
 
+
     }
     public void XOR(){
-
+        system.R[destination] = system.R[a] ^ system.R[b];
+        system.advance();
     }
     public void XORI(){
-
+        system.R[b] = system.R[a] ^ immed;
+        system.advance();
    }
 }
